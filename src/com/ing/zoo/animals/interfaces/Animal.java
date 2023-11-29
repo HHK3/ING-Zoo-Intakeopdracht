5e40ecb5f5d6ef66c06299951e18aedd976ebcb6
Joël/StartZ <joel.lakhai@hva.nl>
2023-11-29 10:54:02 +0100

    feat: addition of interface-files with their own specific methods

---
+++ src/com/ing/zoo/animals/interfaces/Animal.java
@@ -0,0 +1,12 @@
+package com.ing.zoo.animals.interfaces;
+
+/**
+ * Interface class for Animal
+ *
+ * @author Joël Lakhai
+ * @version 1.0
+ */
+public interface Animal {
+    String getName();
+    void sayHello();
+}
---
+++ src/com/ing/zoo/animals/interfaces/Carnivore.java
@@ -0,0 +1,11 @@
+package com.ing.zoo.animals.interfaces;
+
+/**
+ * Interface class for Carnivore Type Animals
+ *
+ * @author Joël Lakhai
+ * @version 1.0
+ */
+public interface Carnivore {
+    void eatMeat();
+}
