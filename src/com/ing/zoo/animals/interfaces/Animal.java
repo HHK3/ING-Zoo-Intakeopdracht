ca63ce905d1f540e895278638d982676a61e5569
Joël/StartZ <joel.lakhai@hva.nl>
2023-11-29 11:16:30 +0100

    feat: addition of setName-method and extra documentation

--- src/com/ing/zoo/animals/interfaces/Animal.java
+++ src/com/ing/zoo/animals/interfaces/Animal.java
@@ -7,6 +7,12 @@ package com.ing.zoo.animals.interfaces;
  * @version 1.0
  */
 public interface Animal {
+    // Method that retrieves the name of the animal
     String getName();
+
+    // Method that sets the name for the animal
+    void setName(String name);
+    
+    // Method that lets the animal say hello
     void sayHello();
 }
